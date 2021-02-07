4303087203686ae2611c03785f94ec9b65cb2833
noriko <aloha.nori1104@gmail.com>
2021-02-07 21:22:22 +0900

    feat: お気に入り機能を作成

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,15 @@
+class FavoritesController < ApplicationController
+  def index
+    @favorites = Favorites.all
+  end
+
+  def create
+    favorite = current_resident.favorites.create(interior_id: params[:interior_id])
+    redirect_to interiors_url, notice: "#{favorite.interior.resident.name}さんのデザインをお気に入り登録しました"
+  end
+  
+  def destroy
+    favorite = current_resident.favorites.find_by(id: params[:id]).destroy
+    redirect_to interiors_url, notice: "#{favorite.interior.resident.name}さんのデザインをお気に入り解除しました"
+  end
+end
--- app/controllers/interiors_controller.rb
+++ app/controllers/interiors_controller.rb
@@ -1,14 +1,58 @@
 class InteriorsController < ApplicationController
+  before_action :set_interior, only: [:edit, :update, :show, :destroy]
+
   def index
-    
+    @interiors = Interior.all
+    # @favorite = current_resident.favorites.find_by(interior_id: @interior.id)
   end
 
   def new
+    @interior = Interior.new
   end
-
-  def show
+  
+  def create
+    @interior = current_resident.interiors.build(interior_params)
+    if params[:back]
+      render :new
+    else
+      if @interior.save
+        redirect_to interiors_path, notice: 'post was successfully created'
+      else
+        render :new
+      end
+    end
   end
 
   def edit
+    if current_resident.id != @interior.resident.id
+      redirect_to interiors_path
+    end
+  end
+
+  def update
+    if @interior.update(interior_params)
+      redirect_to interiors_path, notice: "Successfully edited your post!"
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @favorite = current_resident.favorites.find_by(interior_id: @interior.id)
+  end
+
+  def destroy
+    @interior.destroy
+    redirect_to interiors_path, notice: "Successfully deleted!"
+  end
+
+  private
+
+  def interior_params
+    params.require(@interior).permit(:design_img, :image_cache, :date_of_creation, :budget, :description, :coordinator_id, )
+  end
+
+  def set_interior
+    @interior = Interior.find(params[:id])
   end
 end
