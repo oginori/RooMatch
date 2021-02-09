210a413b19710b907d512df0d4a67f661a83e2ce
noriko <aloha.nori1104@gmail.com>
2021-02-09 22:44:10 +0900

    feat: enum、i18nで日本語対応

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,4 +1,7 @@
 class Contract < ApplicationRecord
   belongs_to :request
   belongs_to :coordinator
+
+  enum approval: { pending: 0, concluded: 1, failure: 2 }
+  enum status: { open: 1, close: 2 }
 end
