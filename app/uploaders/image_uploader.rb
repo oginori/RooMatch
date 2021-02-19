14d8961ce566a9e63dd7e894959b3147fdb89396
noriko <aloha.nori1104@gmail.com>
2021-02-20 04:44:23 +0900

    pending: style: interiors/index.html.erbのスタイル設定中

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,6 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   storage :file
   process :resize_to_fit => [500, 500]
 
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
