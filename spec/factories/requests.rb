c0c5e7cc4406cd6b20f11726f4124fca654c6d89
noriko <aloha.nori1104@gmail.com>
2021-02-21 22:14:16 +0900

    test: suggestion, requestのモデルテストを作成

--- spec/factories/requests.rb
+++ spec/factories/requests.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :request do
     association :resident
 
-    room_size { "MyString" }
+    room_size { "25平米" }
     room_img { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/room01.jpg')) }
     budget { 1 }
     deadline { "2021-02-08" }
--- spec/factories/suggestions.rb
+++ spec/factories/suggestions.rb
@@ -1,8 +1,10 @@
 FactoryBot.define do
   factory :suggestion do
+    association :contract
+    association :request
+
     design_img { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/design01.jpg')) }
     description { "MyText" }
     budget { 100000 }
-    contract { nil }
   end
 end
